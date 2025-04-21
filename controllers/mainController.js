3964b3494cc4b845fdbb15504e151af72938869a
curbaniATU <curbani@atu.edu>
2025-04-21 18:21:19 -0500

    Home Page: Changed formatting of the cards on the page

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -3,8 +3,8 @@ exports.home = (req, res) => {
     title: 'Home', 
     name: 'Caleb Urbani',
     image: 'images/head-shot.jpg',
-    summary: 'My name is Caleb Urbani and I am a Computer Science major at Arkansas Tech University.' +
-      'I started college in 2021 as a Computer Engineering Major before switching to Computer Science in 2023.' +
+    summary: 'My name is Caleb Urbani and I am a Computer Science major at Arkansas Tech University. ' +
+      'I started college in 2021 as a Computer Engineering Major before switching to Computer Science in 2023. ' +
       'I will be graduating in May of 2025. My primary interests are in automation, web development, backend development, and database design.',
     github: 'https://github.com/curbaniATU',
     linkedIn: 'https://www.linkedin.com/in/caleb-urbani-030645354/'
