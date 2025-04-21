3ec9e6d0a277c19a119953cdb7a20d1381dec5d4
curbaniATU <curbani@atu.edu>
2025-04-21 00:54:57 -0500

    Router Setup: Added router to server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const indexRouter = require('./routes/index');
 
 const app = express();
 const PORT = 5000;
@@ -7,6 +8,7 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
+app.use('/', indexRouter);
 
 app.set('view engine', 'ejs');
 
