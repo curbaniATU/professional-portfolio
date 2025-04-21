24cf4a61c96ff626b460c12aed393a0351063e54
curbaniATU <curbani@atu.edu>
2025-04-21 00:48:36 -0500

    Server.js Setup: Initial configuration and setup of server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
